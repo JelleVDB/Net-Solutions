882565a158dca8b600bf77655b4d2e8caa2f6461
Cyberke <jelle1vdb@hotmail.com>
2015-10-09 17:46:12 +0200

    Week 3 - Runtime Compilatie

---
+++ week_03_runtime_compilatie/DrawProgram/DrawProgram/tekenFunctie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawProgram
+{
+    public class tekenFunctie
+    {
+        public static double drawFunction(double dx)
+        {
+            return System.Math.Sin(dx);
+        }
+    }
+}
---
+++ week_03_runtime_compilatie/DrawProgram/DrawProgram/ucFunctie.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DrawProgram
+{
+    class ucFunctie : System.Windows.Controls.Canvas
+    {
+        public ucFunctie()
+        {
+            this.Loaded += UcFunctie_Loaded;
+            this.SizeChanged += UcFunctie_SizeChanged;
+        }
+
+        private void UcFunctie_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            drawFunction();
+        }
+
+        private void UcFunctie_Loaded(object sender, RoutedEventArgs e)
+        {
+            drawFunction();
+        }
+
+        private void drawFunction()
+        {
+            Children.Clear();
+            Point[] points = new Point[(int)ActualWidth];
+            points = calculatePointValuesFromFunction(points, tekenFunctie.drawFunction);
+
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                Line line = new Line();
+                line.X1 = points[i].X;
+                line.X2 = points[i + 1].X;
+                line.Y1 = points[i].Y;
+                line.Y2 = points[i + 1].Y;
+                line.Stroke = Brushes.Red;
+                line.StrokeThickness = 2;
+                Children.Add(line);
+            }
+        }
+
+        private Point[] calculatePointValuesFromFunction(Point[] points, Func<double, double> drawFunction)
+        {
+            double deltaX = 2 * System.Math.PI / ActualWidth;
+
+            for(int i = 0; i< points.Length; i++)
+            {
+                points[i].Y = drawFunction(i * deltaX);
+            }
+
+            double minY = points.Min(p => p.Y);
+            double maxY = points.Max(p => p.Y);
+
+            double deltaY = maxY - minY;
+            double yScale = ActualHeight / deltaY;
+
+            for(int i = 0; i<points.Length; i++)
+            {
+                points[i].X = i;
+                points[i].Y = (maxY - points[i].Y) / (deltaY / ActualHeight);
+            }
+
+            return points;
+        }
+    }
+}
