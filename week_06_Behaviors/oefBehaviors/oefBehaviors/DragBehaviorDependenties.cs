da30bc9b3393de31b07856f03cc48c8ba01ed29f
Cyberke <jelle1vdb@hotmail.com>
2015-11-19 13:24:00 +0100

    Week 6+7 - Behaviors+Templated Controls

---
+++ week_06_Behaviors/oefBehaviors/oefBehaviors/DragBehaviorDependenties.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace oefBehaviors
+{
+    class DragBehaviorDependenties
+    {
+        private Point _elementStartPosition;
+        private Point _mouseStartPosition;
+        private TranslateTransform _tt = new TranslateTransform();
+        private UIElement _uie = null;
+        private Window _parent = null;
+
+
+
+        public static Boolean GetIsDragable(DependencyObject obj)
+        {
+            return (Boolean)obj.GetValue(DragBehaviorDependenties.IsDraggable);
+        }
+
+        public static void SetIsDragable(DependencyObject obj, Boolean value)
+        {
+            obj.SetValue(DragBehaviorDependenties.IsDraggable, value);
+        }
+
+
+        public static readonly DependencyProperty IsDraggable = DependencyProperty.RegisterAttached("IsDraggable", typeof(Boolean), typeof(DragBehaviorDependenties),
+            new UIPropertyMetadata(new PropertyChangedCallback(DragEnabledCb)));
+
+
+        private static void DragEnabledCb(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            UIElement element = target as UIElement;
+
+            if (element == null) return;
+            var behavior = new DragBehaviorDependenties()
+            {
+                _uie = element
+            };
+
+            if (behavior == null) return;
+            if((bool) e.NewValue)
+            {
+                behavior.OnEnabled(element);
+            }
+            else
+            {
+                behavior.OnDisabling(element);
+            }
+        }
+
+        public void OnEnabled(UIElement dependencyObject)
+        {
+            _parent = Application.Current.MainWindow;
+            _uie.RenderTransform = _tt;
+            /*_uie.MouseLeftButtonDown += _uie_MouseLeftButtonDown;
+            _uie.MouseLeftButtonUp += _uie_MouseLeftButtonUp;
+            _uie.MouseMove += _uie_MouseMove;*/
+
+            _uie.AddHandler(UIElement.MouseLeftButtonDownEvent, new MouseButtonEventHandler(_uie_MouseLeftButtonDown), true);
+            _uie.AddHandler(UIElement.MouseMoveEvent, new MouseEventHandler(_uie_MouseMove), true);
+            _uie.AddHandler(UIElement.MouseLeftButtonUpEvent, new MouseButtonEventHandler(_uie_MouseLeftButtonUp), true);
+        }
+
+        public void OnDisabling(UIElement dependencyObject)
+        {
+            /*_uie.MouseLeftButtonDown -= _uie_MouseLeftButtonDown;
+            _uie.MouseLeftButtonUp -= _uie_MouseLeftButtonUp;
+            _uie.MouseMove -= _uie_MouseMove;*/
+
+            _uie.RemoveHandler(UIElement.MouseLeftButtonDownEvent, null);
+            _uie.RemoveHandler(UIElement.MouseMoveEvent, null);
+            _uie.RemoveHandler(UIElement.MouseLeftButtonUpEvent, null);
+
+            _tt = null;
+        }
+
+        private void _uie_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _elementStartPosition = _uie.TranslatePoint(new Point(), _parent);
+            _mouseStartPosition = e.GetPosition(_parent);
+            _uie.CaptureMouse();
+        }
+
+        private void _uie_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var positionDifference = e.GetPosition(_parent) - _mouseStartPosition;
+
+            if (_uie.IsMouseCaptured)
+            {
+                _tt.X = _elementStartPosition.X + positionDifference.X;
+                _tt.Y = _elementStartPosition.Y + positionDifference.Y;
+            }
+        }
+
+        private void _uie_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _uie.ReleaseMouseCapture();
+        }
+
+        
+
+        
+    }
+}
---
+++ week_07_Template_controls/TemplateControlWindow/TemplateControlWindow/ccCaption.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TemplateControlWindow
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:TemplateControlWindow"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:TemplateControlWindow;assembly=TemplateControlWindow"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:ccCaption/>
+    ///
+    /// </summary>
+    /// 
+
+    [TemplatePart(Name = "PART_title", Type = typeof(UIElement))]
+    [TemplatePart(Name = "PART_minimize", Type = typeof(UIElement))]
+    [TemplatePart(Name = "PART_maximize", Type = typeof(UIElement))]
+    [TemplatePart(Name = "PART_close", Type = typeof(UIElement))]
+
+    public class ccCaption : Control
+    {
+
+        static ccCaption()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ccCaption), new FrameworkPropertyMetadata(typeof(ccCaption)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            UIElement doMinimize = GetTemplateChild("PART_minimize") as UIElement;
+            UIElement doMaximize = GetTemplateChild("PART_maximize") as UIElement;
+            UIElement doClose = GetTemplateChild("PART_close") as UIElement;
+
+            if (doMinimize != null)
+            {
+                //doMinimize.MouseUp += DoMinimize_MouseUp;
+                doMinimize.AddHandler(UIElement.MouseUpEvent, new MouseButtonEventHandler(DoMinimize_MouseUp), true);
+            }
+            if(doMaximize != null)
+            {
+                //doMaximize.MouseUp += DoMaximize_MouseUp;
+                doMaximize.AddHandler(UIElement.MouseUpEvent, new MouseButtonEventHandler(DoMaximize_MouseUp), true);
+            }
+            if(doClose != null)
+            {
+                //doClose.MouseUp += DoClose_MouseUp;
+                doClose.AddHandler(UIElement.MouseUpEvent, new MouseButtonEventHandler(DoClose_MouseUp), true);
+            }
+        }
+
+        
+        private void DoMinimize_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Window.GetWindow(this).WindowState = WindowState.Minimized;
+        }
+
+        private void DoMaximize_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(Window.GetWindow(this).WindowState == WindowState.Normal)
+            {
+                Window.GetWindow(this).WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Window.GetWindow(this).WindowState = WindowState.Normal;
+            }
+            
+        }
+
+        private void DoClose_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
+    }
+}
