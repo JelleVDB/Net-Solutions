2e12e03e102dcd369152ac9ebda038acbf129149
Cyberke <jelle1vdb@hotmail.com>
2015-11-20 17:29:23 +0100

    Week 7 - Snippets & Templates

---
+++ week_07_nuget_templates_datatriggers/Snippets/Snippets/DragBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Snippets
+{
+    public class DragBehavior
+    {
+        /*propachanged*/
+        private UIElement _uie = null;
+
+        public static Boolean GetMyMethod(DependencyObject obj)
+        {
+            return (Boolean)obj.GetValue(DragBehavior.MyMethod);
+         }
+
+        public static void SetMyMethod(DependencyObject obj, Boolean value)
+        {
+            obj.SetValue(DragBehavior.MyMethod, value);
+        }
+
+        public static readonly DependencyProperty MyMethod = DependencyProperty.RegisterAttached("MyMethod", typeof(Boolean), typeof(DragBehavior),
+           new UIPropertyMetadata(new PropertyChangedCallback(MyMethodCb)));
+
+        private static void MyMethodCb(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+
+        /*propn*/
+        string myProperty;
+        public string MyProperty
+        {
+            get
+            {
+                return myProperty;
+            }
+            set
+            {
+                if (myProperty != value)
+                {
+                    myProperty = value;
+                    RaisePropertyChanged("MyProperty");
+                }
+            }
+        }
+
+
+        /*raisepc*/
+        void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+    }
+}
---
+++ week_07_nuget_templates_datatriggers/Templates/Templates/NotifyChangedTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Templates
+{
+    class NotifyChangedTemplate : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
+
+/*
+HOE EIGEN TEMPLATE MAKEN
+------------------------
+
+C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\ItemTemplates\CSharp
+Maak map aan met daarin gewenste template
+na afloop, run CMD als admin en navigeer naar de devenv.exe file (in map IDE)
+run devenv.exe /installvstemplates
+*/
